fbe8c8c1abf715812e6f55b2099df2e393f2e121
“Loy” <tanzenchai0923@gmail.com>
2023-12-18 12:08:11 +0800

    first commit

---
+++ src/main/java/com/utm/ip/zebb/ZebbApplication.java
@@ -0,0 +1,13 @@
+package com.utm.ip.zebb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZebbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZebbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utm/ip/zebb/ZebbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utm.ip.zebb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZebbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
